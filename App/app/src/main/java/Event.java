5247f7ae4e819844075ad44ee260e1a87ffabaf3
Wuffluv <rodion.rycha@yandex.ru>
2024-11-13 00:52:22 +0900

    Все сначала

--- App/app/src/main/java/Event.java
+++
@@ -1,8 +0,0 @@
-public class Event {
-    private String title;
-    private String description;
-    private String date;
-    private String urgencyCategory;
-
-    // Конструкторы, геттеры и сеттеры
-}
