64a8f9d485ad1f2403f1b59ead06037b721c7986
Wuffluv <rodion.rycha@yandex.ru>
2024-12-24 00:23:06 +0900

    Маленькое внесение

--- App/app/src/main/java/com/example/todolist/ActivityRegistration.java
+++ App/app/src/main/java/com/example/todolist/ActivityRegistration.java
@@ -1,6 +1,9 @@
 package com.example.todolist;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ActivityRegistration extends AppCompatActivity {
@@ -9,5 +12,18 @@ public class ActivityRegistration extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registration);
+
+        // Инициализация кнопки Registry
+        Button registryButton = findViewById(R.id.RegistryButt);
+
+        // Обработчик нажатия
+        registryButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Переход на MainMenuActivity
+                Intent intent = new Intent(ActivityRegistration.this, MainMenuActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
