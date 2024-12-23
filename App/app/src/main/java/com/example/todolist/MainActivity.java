edd6b382518b783e10db880f2db6c792dd777be1
Wuffluv <rodion.rycha@yandex.ru>
2024-12-24 00:14:55 +0900

    Добавил обработчики для кнопок регистрации и логина

--- App/app/src/main/java/com/example/todolist/MainActivity.java
+++ App/app/src/main/java/com/example/todolist/MainActivity.java
@@ -2,23 +2,47 @@ package com.example.todolist;
 
 import android.os.Bundle;
 
+import android.content.Intent;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        setContentView(R.layout.activity_main); // Это начальный экран
+
+        //Инициализация кнопок
+        Button loginButton = findViewById(R.id.LoginButt); // Кнопка Login
+        Button newUserButton = findViewById(R.id.NewLoginButt); // Кнопка New User
+
+        // Обработчик для кнопки Login
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Переход на mainmenu.xml (MainMenuActivity)
+                Intent intent = new Intent(MainActivity.this, MainMenuActivity.class);
+                startActivity(intent);
+            }
         });
+
+        // Обработчик для кнопки New User
+        newUserButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Переход на activity_registration.xml (ActivityRegistration)
+                Intent intent = new Intent(MainActivity.this, ActivityRegistration.class);
+                startActivity(intent);
+            }
+        });
+
+
     }
 }
